437770d4428a530416cdbed399d1777d51a97aea
tuananh230902 <tuananh230902@gmail.com>
2024-10-16 17:28:36 +0700

    commit Tool IG

---
+++ Tool IG/Form1.Designer.cs
@@ -0,0 +1,263 @@
+﻿namespace Tool_IG
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.tb_User = new System.Windows.Forms.TextBox();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.label2 = new System.Windows.Forms.Label();
+            this.tb_Pass = new System.Windows.Forms.TextBox();
+            this.btStart = new System.Windows.Forms.Button();
+            this.btStop = new System.Windows.Forms.Button();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.tb_UserIG = new System.Windows.Forms.TextBox();
+            this.tb_PassIG = new System.Windows.Forms.TextBox();
+            this.my_username = new System.Windows.Forms.Label();
+            this.my_coins = new System.Windows.Forms.Label();
+            this.ID = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.NV = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.account_username = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.URL = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.massage = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 47);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(57, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "UserName";
+            // 
+            // tb_User
+            // 
+            this.tb_User.Location = new System.Drawing.Point(12, 63);
+            this.tb_User.Name = "tb_User";
+            this.tb_User.Size = new System.Drawing.Size(156, 20);
+            this.tb_User.TabIndex = 1;
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.AllowUserToAddRows = false;
+            this.dataGridView1.AllowUserToDeleteRows = false;
+            this.dataGridView1.BackgroundColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.ID,
+            this.NV,
+            this.account_username,
+            this.URL,
+            this.massage});
+            this.dataGridView1.Location = new System.Drawing.Point(15, 202);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.ReadOnly = true;
+            this.dataGridView1.Size = new System.Drawing.Size(543, 301);
+            this.dataGridView1.TabIndex = 3;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(183, 47);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(53, 13);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Password";
+            // 
+            // tb_Pass
+            // 
+            this.tb_Pass.Location = new System.Drawing.Point(186, 63);
+            this.tb_Pass.Name = "tb_Pass";
+            this.tb_Pass.Size = new System.Drawing.Size(156, 20);
+            this.tb_Pass.TabIndex = 5;
+            this.tb_Pass.UseSystemPasswordChar = true;
+            // 
+            // btStart
+            // 
+            this.btStart.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(128)))), ((int)(((byte)(255)))), ((int)(((byte)(128)))));
+            this.btStart.Location = new System.Drawing.Point(477, 26);
+            this.btStart.Name = "btStart";
+            this.btStart.Size = new System.Drawing.Size(81, 57);
+            this.btStart.TabIndex = 2;
+            this.btStart.Text = "Start";
+            this.btStart.UseVisualStyleBackColor = false;
+            this.btStart.Click += new System.EventHandler(this.btStart_Click);
+            // 
+            // btStop
+            // 
+            this.btStop.BackColor = System.Drawing.Color.Red;
+            this.btStop.Location = new System.Drawing.Point(477, 113);
+            this.btStop.Name = "btStop";
+            this.btStop.Size = new System.Drawing.Size(81, 57);
+            this.btStop.TabIndex = 6;
+            this.btStop.Text = "Stop";
+            this.btStop.UseVisualStyleBackColor = false;
+            this.btStop.Click += new System.EventHandler(this.btStop_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(12, 135);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(71, 13);
+            this.label3.TabIndex = 7;
+            this.label3.Text = "UserName IG";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(183, 136);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(67, 13);
+            this.label4.TabIndex = 8;
+            this.label4.Text = "Password IG";
+            // 
+            // tb_UserIG
+            // 
+            this.tb_UserIG.Location = new System.Drawing.Point(12, 151);
+            this.tb_UserIG.Name = "tb_UserIG";
+            this.tb_UserIG.Size = new System.Drawing.Size(156, 20);
+            this.tb_UserIG.TabIndex = 9;
+            // 
+            // tb_PassIG
+            // 
+            this.tb_PassIG.Location = new System.Drawing.Point(186, 151);
+            this.tb_PassIG.Name = "tb_PassIG";
+            this.tb_PassIG.Size = new System.Drawing.Size(156, 20);
+            this.tb_PassIG.TabIndex = 10;
+            this.tb_PassIG.UseSystemPasswordChar = true;
+            // 
+            // my_username
+            // 
+            this.my_username.AutoSize = true;
+            this.my_username.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.my_username.ForeColor = System.Drawing.Color.Red;
+            this.my_username.Location = new System.Drawing.Point(362, 67);
+            this.my_username.Name = "my_username";
+            this.my_username.Size = new System.Drawing.Size(85, 16);
+            this.my_username.TabIndex = 12;
+            this.my_username.Text = "User Name";
+            // 
+            // my_coins
+            // 
+            this.my_coins.AutoSize = true;
+            this.my_coins.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.my_coins.ForeColor = System.Drawing.Color.Red;
+            this.my_coins.Location = new System.Drawing.Point(378, 155);
+            this.my_coins.Name = "my_coins";
+            this.my_coins.Size = new System.Drawing.Size(51, 16);
+            this.my_coins.TabIndex = 13;
+            this.my_coins.Text = "0 VND";
+            // 
+            // ID
+            // 
+            this.ID.HeaderText = "#";
+            this.ID.Name = "ID";
+            this.ID.ReadOnly = true;
+            this.ID.Width = 50;
+            // 
+            // NV
+            // 
+            this.NV.HeaderText = "Loại nhiệm vụ";
+            this.NV.Name = "NV";
+            this.NV.ReadOnly = true;
+            // 
+            // account_username
+            // 
+            this.account_username.HeaderText = "Tài khoản Instagram";
+            this.account_username.Name = "account_username";
+            this.account_username.ReadOnly = true;
+            this.account_username.Width = 150;
+            // 
+            // URL
+            // 
+            this.URL.HeaderText = "Link nhiệm vụ";
+            this.URL.Name = "URL";
+            this.URL.ReadOnly = true;
+            // 
+            // massage
+            // 
+            this.massage.HeaderText = "Trạng thái";
+            this.massage.Name = "massage";
+            this.massage.ReadOnly = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(583, 541);
+            this.Controls.Add(this.my_coins);
+            this.Controls.Add(this.my_username);
+            this.Controls.Add(this.tb_PassIG);
+            this.Controls.Add(this.tb_UserIG);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.btStop);
+            this.Controls.Add(this.tb_Pass);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.btStart);
+            this.Controls.Add(this.tb_User);
+            this.Controls.Add(this.label1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Tool ig";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox tb_User;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox tb_Pass;
+        private System.Windows.Forms.Button btStart;
+        private System.Windows.Forms.Button btStop;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox tb_UserIG;
+        private System.Windows.Forms.TextBox tb_PassIG;
+        private System.Windows.Forms.Label my_username;
+        private System.Windows.Forms.Label my_coins;
+        private System.Windows.Forms.DataGridViewTextBoxColumn ID;
+        private System.Windows.Forms.DataGridViewTextBoxColumn NV;
+        private System.Windows.Forms.DataGridViewTextBoxColumn account_username;
+        private System.Windows.Forms.DataGridViewTextBoxColumn URL;
+        private System.Windows.Forms.DataGridViewTextBoxColumn massage;
+    }
+}
+
---
+++ Tool IG/Form1.cs
@@ -0,0 +1,269 @@
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Tool_IG
+{
+    public partial class Form1 : Form
+    {
+
+        bool run = false;
+        string file_accounts_path = "accounts.txt";
+        ChromeDriver driverig;
+        ChromeDriver driverinss;
+        public Form1()
+        {
+            InitializeComponent();
+            dataGridView1.AllowUserToResizeColumns = false;
+            if (File.Exists(file_accounts_path))
+            {
+                string data_acc = File.ReadAllText(file_accounts_path);
+                if(!string.IsNullOrEmpty(data_acc))
+                {
+                    tb_User.Text = Regex.Match(data_acc, @"username_vipig=(.*?);").Groups[1].Value;
+                    tb_Pass.Text = Regex.Match(data_acc, @"password_vipig=(.*?);").Groups[1].Value;
+                    tb_UserIG.Text = Regex.Match(data_acc, @"username_ig_1=(.*?);").Groups[1].Value;
+                    tb_PassIG.Text = Regex.Match(data_acc, @"password_ig_1=(.*?);").Groups[1].Value;
+
+                }
+            }
+            else
+            {
+                File.Create(file_accounts_path);
+            }
+
+        }
+
+
+        private void btStop_Click(object sender, EventArgs e)
+        {
+            run = false; 
+        }
+
+        private void btStart_Click(object sender, EventArgs e)
+        {
+            run = true;
+            string username_tool = tb_User.Text.Trim();
+            string password_tool = tb_Pass.Text.Trim();
+            string username_ig = tb_UserIG.Text.Trim();
+            string password_ig = tb_PassIG.Text.Trim();
+            
+            if(!string.IsNullOrEmpty(username_tool) && !string.IsNullOrEmpty(password_tool) && !string.IsNullOrEmpty(username_ig) && !string.IsNullOrEmpty(password_ig))
+            {
+                Thread t = new Thread(() =>
+                {
+                    ChromeDriver driverVipig = OpenChrome("https://vipig.net/home.php");
+                    if(driverVipig != null)
+                    {
+                        delay(2);
+                        driverVipig.FindElement(By.CssSelector("button.btn.btn-primary[data-dismiss='modal']")).Click();
+                        delay(1);
+                        driverVipig.FindElement(By.CssSelector("input[name='username']")).Clear();
+                        driverVipig.FindElement(By.CssSelector("input[name='username']")).SendKeys(username_tool);
+
+                        driverVipig.FindElement(By.CssSelector("input[name='password']")).Clear();
+                        driverVipig.FindElement(By.CssSelector("input[name='password']")).SendKeys(password_tool);
+
+                        driverVipig.FindElement(By.CssSelector("input[name='submit']")).Click();
+
+                        delay(2);
+                        bool islogin = false;
+                        try
+                        {
+                            var alert = driverVipig.SwitchTo().Alert();
+                            string texterr = alert.Text;
+                            alert.Accept();
+                            driverVipig.Quit();
+                            show_error(texterr);
+                        }
+                        catch
+                        {
+                             islogin = true;
+                        }
+                        if (islogin)
+                        {
+                            this.Invoke(new Action(() =>
+                            {
+                                //lấy thông tin username password của instagram để đăng nhập vào 
+                                my_username.Text = driverVipig.FindElement(By.XPath("/html/body/div/div/div[1]/h2/i")).Text;
+                                my_coins.Text = driverVipig.FindElement(By.Id("soduchinh")).Text+ "VND";
+                            }));
+                            ChromeDriver driverins = OpenChrome("https://www.instagram.com/");
+                            delay(2);
+                            driverins.FindElement(By.Name("username")).SendKeys(username_ig);
+                            driverins.FindElement(By.Name("password")).SendKeys(password_ig);
+                            delay(2);
+                            driverins.FindElement(By.XPath("/html/body/div[2]/div/div/div[2]/div/div/div[1]/section/main/article/div[2]/div[1]/div[2]/form/div/div[3]/button/div")).Click();
+                            
+                            Thread.Sleep(TimeSpan.FromSeconds(5));
+                            if(check(driverins, By.XPath("/html/body/div[3]/div[1]/div/div[2]/div/div/div/div/div[2]/div/div/div[3]/button[2]")))
+                            {
+                                driverins.FindElement(By.XPath("/html/body/div[3]/div[1]/div/div[2]/div/div/div/div/div[2]/div/div/div[3]/button[2]")).Click();
+                            }
+                            else
+                            {
+                                if(check(driverins, By.Id("slfErrorAlert"))) {
+                                    show_error(driverins.FindElement(By.Id("slErrorAlert")).Text);
+                                    driverins.Quit();
+                                    driverVipig.Quit();
+                                    return;
+                                }
+                            }
+                            if (!run)
+                            {
+                                driverVipig.Quit();
+                                driverins.Quit();return;
+                            }
+                            driverinss = driverins;
+                            driverig = driverVipig;
+                            while (run)
+                            {
+                                if (!run)
+                                {
+                                    driverVipig.Quit();
+                                    driverins.Quit(); return;
+                                    //var radom = new Random();
+                                    ////int rand = radom.Next(1, 3);
+                                    //int rand = 1;
+                                    //if(rand ==1)
+                                    //{
+                                    //    //like
+                                    //    start_like();
+                                    //}else if (rand == 2)
+                                    //{
+                                    //    //fl
+                                    //}else if (rand == 3)
+                                    //{
+                                    //    //cmt
+                                    //}
+                                }
+                                var random  = new Random();
+                                //int  rand = random.Next(1, 3);
+                                int rand = 1;
+                                if (rand == 1)
+                                {
+                                    start_like();
+                                    //like
+                                }else if (rand == 2)
+                                {
+                                    //fl
+                                }else if (rand == 3)
+                                {
+                                    //cmt
+                                }
+                            }
+ 
+
+                        }
+
+                    }
+                    else
+                    {
+                        show_error("Thay đổi phiên bản chrome");
+                    }
+                });
+                t.IsBackground = true;
+                t.Start();
+            }
+            else
+            {
+                show_error("Nhập đầy đủ thông tin");
+            }
+        }
+        void show_error(string message)
+        {
+            MessageBox.Show(message, "Thất Bại", MessageBoxButtons.OK,MessageBoxIcon.Error);
+        }
+
+        //Open Chrome
+         ChromeDriver OpenChrome(string url, bool show = true)
+        {
+            ChromeOptions options = new ChromeOptions();
+            if (!show)
+            {
+                options.AddArgument("headless");
+            }
+            ChromeDriverService service;
+            try
+            {
+                service = ChromeDriverService.CreateDefaultService();
+                service.HideCommandPromptWindow = true;
+            }
+            catch 
+            {
+                return null;
+            }
+            ChromeDriver driver = new ChromeDriver(service, options);
+            try
+            {
+                driver.Url = url;
+                driver.Navigate();
+                return driver;
+            }
+            catch 
+            {
+                return null;
+            }  
+            
+
+        }
+
+        void delay(int s)
+        {
+            Thread.Sleep(TimeSpan.FromSeconds(s));
+        }
+        bool check(ChromeDriver driver, By by)
+        {
+            try
+            {
+                driver.FindElement(by);
+                return true;
+            }catch 
+            {
+                return false;
+            }
+        }
+        private void start_like()
+        {
+            driverig.Url = "https://vipig.net/kiemtien/";
+            driverig.Navigate();
+            delay(2);
+            var all_jobs = driverig.FindElement(By.Id("dspost")).FindElements(By.TagName("button"));
+            foreach (var button in all_jobs)
+            {
+                button.Click();
+                string url = button.GetAttribute("title").Replace("", "");
+                driverinss.Url = url;
+                driverinss.Navigate();
+                delay(2);
+                try 
+                {
+                    var elements = driverinss.FindElements(By.ClassName("wp06b"));
+                    if (elements.Count > 1)
+                    {
+                        elements[1].Click();
+                    }
+                    //driverinss.FindElement(By.XPath("/html/body/div[2]/div/div/div[2]/div/div/div[1]/div[1]/div[2]/section/main/div[1]/div/div/div[2]/div/div[1]/div/article[1]/div/div[3]/div/div/div[1]/div[1]/span[1]/div")).Click();
+                }
+                catch { }
+                driverig.SwitchTo().Window(driverig.WindowHandles.Last());
+                driverig.Close();
+                delay(2);
+                string id = url.Substring(28).Trim();
+                driverig.FindElement(By.Id(id)).Click();
+                delay(5);
+            }
+        }
+    }
+}
